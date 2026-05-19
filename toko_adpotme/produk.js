c84ce16e13886677a4bdeebc3d4275c480b9801c
devita-collab <devitasusantoputri1362009@gmail.com>
2026-05-19 21:20:47 +0700

    toko_adoptme

---
+++ toko_adpotme/produk.js
@@ -0,0 +1,178 @@
+const daftarProduk = [
+    {
+        id: "prod-1",
+        nama: "mochi meow N",
+        harga: 7000,
+        stok: 1,
+        gambar: "gambar/mochi_meow.png",
+        placeholder: "Masukkan username roblox"
+    },
+    {
+        id: "prod-2",
+        nama: "blue jay N",
+        harga: 4000,
+        stok: 1,
+        gambar: "gambar/blue_jay.png",
+        placeholder: "Masukkan username roblox"
+    },
+    {
+        id: "prod-3",
+        nama: "blue whale N",
+        harga: 15000,
+        stok: 1,
+        gambar: "gambar/blue_whale.png",
+        placeholder: "Masukkan username roblox"
+    },
+    {
+        id: "prod-4",
+        nama: "cold cube N",
+        harga: 10000,
+        stok: 1,
+        gambar: "gambar/cold_cube.png",
+        placeholder: "Masukkan username roblox"
+    },
+    {
+        id: "prod-4",
+        nama: "scarecrow horse N",
+        harga: 10000,
+        stok: 1,
+        gambar: "gambar/scarecrow_horse.png",
+        placeholder: "masukkan username roblox"
+    },
+    {
+        id: "prod-5",
+        nama: "tree frog N",
+        harga: 4000,
+        stok: 1,
+        gambar: "gambar/tree_frog.png",
+        placeholder: "masukkan username roblox"
+    },
+    {
+        id: "prod-6",
+        nama: "Velociraptor N",
+        harga: 7000,
+        stok: 1,
+        gambar: "gambar/Velociraptor.png",
+        placeholder: "masukkan username roblox"
+    },
+    {
+        id: "prod-7",
+        nama: "pangolin N",
+        harga: 8000,
+        stok: 1,
+        gambar: "gambar/pangolin.png",
+        placeholder: "masukkan username roblox"
+    },
+    {
+        id: "prod-8",
+        nama: "ankylosaurus N",
+        harga: 7000,
+        stok: 1,
+        gambar: "gambar/ankylosaurus.png",
+        placeholder: "masukkan username roblox"
+    },
+    {
+        id: "prod-9",
+        nama: "ocelot N",
+        harga: 7000,
+        stok: 1,
+        gambar: "gambar/ocelot.png",
+        placeholder: "masukkan username roblox"
+    },
+    {
+        id: "prod-10",
+        nama: "Castle Hermit Crab",
+        harga: 1000,
+        stok: 1,
+        gambar: "gambar/Castle Hermit Crab.png",
+        placeholder: "masukkan username roblox"
+    },
+    {
+        id: "prod-11",
+        nama: "Ice Cream Hermit Crab",
+        harga: 2000,
+        stok: 0,
+        gambar: "gambar/Ice Cream Hermit Crab.png",
+        placeholder: "masukkan username roblox"
+    },
+    {
+        id: "prod-12",
+        nama: "Banded Palm Civet N",
+        harga: 5000,
+        stok: 1,
+        gambar: "gambar/Banded Palm Civet.png",
+        placeholder: "masukkan username roblox"
+    },
+    {
+        id: "prod-13",
+        nama: "Eel N",
+        harga: 5000,
+        stok: 1,
+        gambar: "gambar/Eel.png",
+        placeholder: "masukkan username roblox"
+    },
+    {
+        id: "prod-14",
+        nama: "Fossa N",
+        harga: 10000,
+        stok: 1,
+        gambar: "gambar/Fossa.png",
+        placeholder: "masukkan username roblox"
+    },
+    {
+        id: "prod-15",
+        nama: "Jumping Spider N",
+        harga: 12000,
+        stok: 1,
+        gambar: "gambar/Jumping Spider.png",
+        placeholder: "masukkan username roblox"
+    },
+    {
+        id: "prod-16",
+        nama: "Easter Eggy Box",
+        harga: 5000,
+        stok: 1,
+        gambar: "gambar/Easter Eggy Box.png",
+        placeholder: "masukkan username roblox"
+    },
+    {
+        id: "prod-17",
+        nama: "Halloween Chick Box",
+        harga: 7000,
+        stok: 1,
+        gambar: "gambar/Halloween Chick Box.png",
+        placeholder: "masukkan username roblox"
+    },
+    {
+        id: "prod-18",
+        nama: "Hermit Crab Box",
+        harga: 12000,
+        stok: 2,
+        gambar: "gambar/Hermit Crab Box.png",
+        placeholder: "masukkan username roblox"
+    },
+    {
+        id: "prod-19",
+        nama: "Scarecrow Box",
+        harga: 3000,
+        stok: 1,
+        gambar: "gambar/Scarecrow Box.png",
+        placeholder: "masukkan username roblox"
+    },
+     {
+        id: "prod-20",
+        nama: "Dapper Friend Carrier",
+        harga: 5000,
+        stok: 1,
+        gambar: "gambar/Dapper Friend Carrier.png",
+        placeholder: "masukkan username roblox"
+    },
+     {
+        id: "prod-21",
+        nama: "Canine Cruiser",
+        harga: 5000,
+        stok: 1,
+        gambar: "gambar/Canine Cruiser.png",
+        placeholder: "masukkan username roblox"
+    },
+];
