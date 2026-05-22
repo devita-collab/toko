d42a05cdb6a939f724028895ce7700a661afbcc0
devita-collab <devitasusantoputri1362009@gmail.com>
2026-05-22 10:00:40 +0700

    Add files via upload

---
+++ produk-item.js
@@ -0,0 +1,107 @@
+const produkItem = [
+    {
+        id: "item-1",
+        nama: "Easter Eggy Box",
+        harga: 5000,
+        stok: 1,
+        gambar: "gambar/Easter Eggy Box.png"
+    },
+    {
+        id: "item-2",
+        nama: "Halloween Chick Box",
+        harga: 7000,
+        stok: 1,
+        gambar: "gambar/Halloween Chick Box.png"
+    },
+    {
+        id: "item-3",
+        nama: "Hermit Crab Box",
+        harga: 12000,
+        stok: 2,
+        gambar: "gambar/Hermit Crab Box.png"
+    },
+    {
+        id: "item-4",
+        nama: "Scarecrow Box",
+        harga: 3000,
+        stok: 1,
+        gambar: "gambar/Scarecrow Box.png"
+    },
+    {
+        id: "item-5",
+        nama: "Dapper Friend Carrier",
+        harga: 5000,
+        stok: 1,
+        gambar: "gambar/Dapper Friend Carrier.png"
+    },
+    {
+        id: "item-6",
+        nama: "Canine Cruiser",
+        harga: 5000,
+        stok: 1,
+        gambar: "gambar/Canine Cruiser.png"
+    },
+    {
+        id: "item-7",
+        nama: "cat hood",
+        harga: 2000,
+        stok: 1,
+        gambar: "gambar/cat hood.png"
+    },
+    {
+        id: "item-8",
+        nama: "Charon's Boat",
+        harga: 6000,
+        stok: 1,
+        gambar: "gambar/Charon's Boat.png"
+    },
+    {
+        id: "item-9",
+        nama: "Hot Rod Sleigh",
+        harga: 7000,
+        stok: 1,
+        gambar: "gambar/Hot Rod Sleigh.png"
+    },
+    {
+        id: "item-10",
+        nama: "Rocket Ship Hat",
+        harga: 10000,
+        stok: 1,
+        gambar: "gambar/Rocket Ship Hat.png"
+    },
+    {
+        id: "item-11",
+        nama: "Compass",
+        harga: 3000,
+        stok: 1,
+        gambar: "gambar/Compass.png"
+    },
+    {
+        id: "item-12",
+        nama: "Haunted Wagon",
+        harga: 3500,
+        stok: 1,
+        gambar: "gambar/Haunted Wagon.png"
+    },
+    {
+        id: "item-13",
+        nama: "Claw Machine Stroller",
+        harga: 2000,
+        stok: 1,
+        gambar: "gambar/Claw Machine Stroller.png"
+    },
+    {
+        id: "item-14",
+        nama: "Cupcake Hat",
+        harga: 2000,
+        stok: 1,
+        gambar: "gambar/Cupcake Hat.png"
+    },
+    {
+        id: "item-15",
+        nama: "Eco Red Apple Basket Hat",
+        harga: 1500,
+        stok: 1,
+        gambar: "gambar/Eco Red Apple Basket Hat.png"
+    },
+];
---
+++ produk-pet.js
@@ -0,0 +1,212 @@
+const produkPet = [
+    {
+        id: "pet-1",
+        nama: "mochi meow N",
+        harga: 7000,
+        stok: 1,
+        gambar: "gambar/mochi_meow.png"
+    },
+    {
+        id: "pet-2",
+        nama: "blue jay N",
+        harga: 4000,
+        stok: 1,
+        gambar: "gambar/blue_jay.png"
+    },
+    {
+        id: "pet-3",
+        nama: "blue whale N",
+        harga: 7000,
+        stok: 1,
+        gambar: "gambar/blue_whale.png"
+    },
+    {
+        id: "pet-4",
+        nama: "cold cube N",
+        harga: 6000,
+        stok: 1,
+        gambar: "gambar/cold_cube.png"
+    },
+    {
+        id: "pet-5",
+        nama: "scarecrow horse N",
+        harga: 6000,
+        stok: 1,
+        gambar: "gambar/scarecrow_horse.png"
+    },
+    {
+        id: "pet-6",
+        nama: "tree frog N",
+        harga: 4000,
+        stok: 1,
+        gambar: "gambar/tree_frog.png"
+    },
+    {
+        id: "pet-7",
+        nama: "Velociraptor N",
+        harga: 5000,
+        stok: 1,
+        gambar: "gambar/Velociraptor.png"
+    },
+    {
+        id: "pet-8",
+        nama: "pangolin N",
+        harga: 5000,
+        stok: 1,
+        gambar: "gambar/pangolin.png"
+    },
+    {
+        id: "pet-9",
+        nama: "ankylosaurus N",
+        harga: 5000,
+        stok: 1,
+        gambar: "gambar/ankylosaurus.png"
+    },
+    {
+        id: "pet-10",
+        nama: "ocelot N",
+        harga: 5000,
+        stok: 1,
+        gambar: "gambar/ocelot.png"
+    },
+    {
+        id: "pet-11",
+        nama: "Castle Hermit Crab",
+        harga: 1000,
+        stok: 1,
+        gambar: "gambar/Castle Hermit Crab.png"
+    },
+    {
+        id: "pet-12",
+        nama: "Tegu N",
+        harga: 4000,
+        stok: 1,
+        gambar: "gambar/Tegu.png"
+    },
+    {
+        id: "pet-13",
+        nama: "Banded Palm Civet N",
+        harga: 4000,
+        stok: 1,
+        gambar: "gambar/Banded Palm Civet.png"
+    },
+    {
+        id: "pet-14",
+        nama: "Eel N",
+        harga: 4000,
+        stok: 1,
+        gambar: "gambar/Eel.png"
+    },
+    {
+        id: "pet-15",
+        nama: "Fossa N",
+        harga: 4000,
+        stok: 1,
+        gambar: "gambar/Fossa.png"
+    },
+    {
+        id: "pet-16",
+        nama: "Jumping Spider N",
+        harga: 5000,
+        stok: 1,
+        gambar: "gambar/Jumping Spider.png"
+    },
+    {
+        id: "pet-17",
+        nama: "mole N",
+        harga: 4000,
+        stok: 1,
+        gambar: "gambar/mole.png"
+    },
+    {
+        id: "pet-18",
+        nama: "angelfish N",
+        harga: 5000,
+        stok: 1,
+        gambar: "gambar/angelfish.png"
+    },
+    {
+        id: "pet-19",
+        nama: "garden snake N",
+        harga: 3000,
+        stok: 1,
+        gambar: "gambar/garden snake.png"
+    },
+    {
+        id: "pet-20",
+        nama: "frankenfeline N",
+        harga: 4000,
+        stok: 1,
+        gambar: "gambar/frankenfeline.png"
+    },
+    {
+        id: "pet-21",
+        nama: "striped eggy N",
+        harga: 7000,
+        stok: 1,
+        gambar: "gambar/striped eggy.png"
+    },
+    {
+        id: "pet-22",
+        nama: "Cockroach N",
+        harga: 3000,
+        stok: 1,
+        gambar: "gambar/Cockroach.png"
+    },
+    {
+        id: "pet-23",
+        nama: "Purrowl",
+        harga: 3000,
+        stok: 1,
+        gambar: "gambar/Purrowl.png"
+    },
+    {
+        id: "pet-24",
+        nama: "English Sheepdog",
+        harga: 2500,
+        stok: 1,
+        gambar: "gambar/English Sheepdog.png"
+    },
+    {
+        id: "pet-25",
+        nama: "Red Panda",
+        harga: 1000,
+        stok: 1,
+        gambar: "gambar/Red Panda.png"
+    },
+    {
+        id: "pet-26",
+        nama: "Starfish",
+        harga: 2000,
+        stok: 4,
+        gambar: "gambar/Starfish.png"
+    },
+    {
+        id: "pet-27",
+        nama: "Clover Cow",
+        harga: 5000,
+        stok: 1,
+        gambar: "gambar/Clover Cow.png"
+    },
+    {
+        id: "pet-28",
+        nama: "Wolpertinger",
+        harga: 1000,
+        stok: 2,
+        gambar: "gambar/Wolpertinger.png"
+    },
+    {
+        id: "pet-29",
+        nama: "Brachiosaurus",
+        harga: 3000,
+        stok: 6,
+        gambar: "gambar/Brachiosaurus.png"
+    },
+    {
+        id: "pet-30",
+        nama: "Mr. Whiskerpips",
+        harga: 12000,
+        stok: 1,
+        gambar: "gambar/Mr. Whiskerpips.png"
+    }
+];
